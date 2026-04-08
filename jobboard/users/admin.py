43d35fbb550c223caf02e1ca9ba237964176338c
Young <denis-belousov20@yandex.ru>
2026-04-08 19:55:15 +0300

    upd class profile v adminke

--- jobboard/users/admin.py
+++ jobboard/users/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import Profile
 # Register your models here.
 
-admin.site.register(Profile)
+#admin.site.register(Profile)
+
+@admin.register(Profile)
+class AdminProfile(admin.ModelAdmin):
+    list_display = ("user", "phone", "location", "is_verified", "created_at")
+    list_filter = ("is_verified", "created_at")
+    search_fields = ("user__username", "phone", "location")
 
