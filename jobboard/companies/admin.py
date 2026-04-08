b9e40b72c4b6f1ea9cc590b7606f93996d257ef3
Young <denis-belousov20@yandex.ru>
2026-04-08 20:26:59 +0300

    zaregistriroval company v adminke

--- jobboard/companies/admin.py
+++ jobboard/companies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Company
 # Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'website', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name','owner__username')
+
