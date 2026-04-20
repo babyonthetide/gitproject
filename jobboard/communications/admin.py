defefa9758c2d28334f0f96a7a2a1e0d1bbc3a5c
Young <denis-belousov20@yandex.ru>
2026-04-20 15:34:51 +0300

    Зарегистрировал модель приглашений в админке

--- jobboard/communications/admin.py
+++ jobboard/communications/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Response
+from .models import Response,Invitation
 
 @admin.register(Response)
 class AdminResponse(admin.ModelAdmin):
@@ -8,3 +8,8 @@ class AdminResponse(admin.ModelAdmin):
     search_fields = ('vacancy__title','resume__last_name')
 
 
+@admin.register(Invitation)
+class AdminInvitation(admin.ModelAdmin):
+    list_display = ('id','vacancy','resume','status','created_at')
+    list_filter = ('status','created_at')
+    search_fields = ('vacancy__title','resume__last_name')
