825591faf185f80a17352f56b79191496f477815
Young <denis-belousov20@yandex.ru>
2026-04-20 15:25:59 +0300

    Зарегистрировал модель отликов в админке

--- jobboard/communications/admin.py
+++ jobboard/communications/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Response
 
-# Register your models here.
+@admin.register(Response)
+class AdminResponse(admin.ModelAdmin):
+    list_display = ('vacancy','resume','status','created_at')
+    list_filter = ('status','created_at')
+    search_fields = ('vacancy__title','resume__last_name')
+
+
