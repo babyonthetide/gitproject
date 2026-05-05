f1b9d09d15597da2a3c25d8fb2453133af3a2022
Young <denis-belousov20@yandex.ru>
2026-05-05 12:37:04 +0300

    обновил валидаторы размера изображений(ранее были написаны криво)

--- jobboard/companies/validators.py
+++ jobboard/companies/validators.py
@@ -3,10 +3,9 @@ from django.core.exceptions import ValidationError
 
 
 def validate_logo_size(image):
-    size = os.path.getsize(image)
-    size_mbites = size/(1024*1024)
-    max_size = 8
-    if size_mbites and size_mbites>max_size:
+    size = image.size
+    max_size = 8 * 1024 * 1024
+    if size and size > max_size:
         raise ValidationError('Размер изображения превышает 8 Мбайт')
 
 def logo_upload_path(instance, filename):
