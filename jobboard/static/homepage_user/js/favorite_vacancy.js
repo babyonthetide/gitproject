22f12a8cade72d9ab67410ff33f7bf857a8cb2ca
Young <denis-belousov20@yandex.ru>
2026-05-07 16:14:55 +0300

    Исправил gitingore,ранее не добавлялся js скрипт для механики избранной вакансии и css стили

---
+++ jobboard/static/homepage_user/js/favorite_vacancy.js
@@ -0,0 +1,44 @@
+// favorite_vacancy.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".favorite-icon").forEach(icon => {
+    icon.addEventListener("click", () => {
+      const vacancyId = icon.dataset.id;
+
+      fetch(`/vacancy/toggle/${vacancyId}/`, {
+        method: "POST",
+        headers: {
+          "X-CSRFToken": getCookie("csrftoken"),  
+          "X-Requested-With": "XMLHttpRequest"
+        }
+      })
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === "added") {
+          icon.classList.remove("bi-star");
+          icon.classList.add("bi-star-fill", "text-warning");
+        } else if (data.status === "removed") {
+          icon.classList.remove("bi-star-fill", "text-warning");
+          icon.classList.add("bi-star");
+        }
+      });
+    });
+  });
+
+  function getCookie(name) {
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== "") {
+    const cookies = document.cookie.split(";");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
+}
+
+});
+
