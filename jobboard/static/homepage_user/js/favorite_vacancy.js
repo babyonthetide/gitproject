4933c1e37975aa1e1a151577273b44a38926dd75
Young <denis-belousov20@yandex.ru>
2026-05-26 15:26:43 +0300

    фикс прошлого коммита(поведение кнопки добавить в избранное

--- jobboard/static/homepage_user/js/favorite_vacancy.js
+++ jobboard/static/homepage_user/js/favorite_vacancy.js
@@ -1,5 +1,3 @@
-// favorite_vacancy.js
-
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".favorite-icon").forEach(icon => {
     icon.addEventListener("click", () => {
@@ -8,37 +6,38 @@ document.addEventListener("DOMContentLoaded", () => {
       fetch(`/vacancy/toggle/${vacancyId}/`, {
         method: "POST",
         headers: {
-          "X-CSRFToken": getCookie("csrftoken"),  
+          "X-CSRFToken": getCookie("csrftoken"),
           "X-Requested-With": "XMLHttpRequest"
         }
       })
       .then(res => res.json())
       .then(data => {
-        if (data.status === "added") {
-          icon.classList.remove("bi-star");
-          icon.classList.add("bi-star-fill", "text-warning");
-        } else if (data.status === "removed") {
-          icon.classList.remove("bi-star-fill", "text-warning");
-          icon.classList.add("bi-star");
-        }
+        // Находим все иконки с этим vacancyId
+        document.querySelectorAll(`.favorite-icon[data-id="${vacancyId}"]`).forEach(el => {
+          if (data.status === "added") {
+            el.classList.remove("bi-star");
+            el.classList.add("bi-star-fill", "text-warning");
+          } else if (data.status === "removed") {
+            el.classList.remove("bi-star-fill", "text-warning");
+            el.classList.add("bi-star");
+          }
+        });
       });
     });
   });
 
   function getCookie(name) {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== "") {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.substring(0, name.length + 1) === (name + "=")) {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        if (cookie.substring(0, name.length + 1) === (name + "=")) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
       }
     }
+    return cookieValue;
   }
-  return cookieValue;
-}
-
 });
-
